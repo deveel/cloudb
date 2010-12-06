32c610d6aa2cad58ddf00f4e98d6c6969b27e4d5
Antonello Provenzano <antonello@deveel.com>
2010-12-06 17:37:43 +0000

    - Solution organization - NAnt build scripting

--- cloudb-nunit/Deveel.Data.Net.Client/XmlRpcMessageSerializerTest.cs
+++ cloudb-nunit/Deveel.Data.Net.Client/XmlRpcMessageSerializerTest.cs
@@ -2,34 +2,32 @@
 using System.IO;
 using System.Text;
 
-using Deveel.Data.Net.Client;
-
 using NUnit.Framework;
 
-namespace Deveel.Data.Net {
+namespace Deveel.Data.Net.Client {
 	[TestFixture]
-	public class JsonRpcMessageSerializerTest {
+	public class XmlRpcMessageSerializerTest {
 		private static string Serialize(Message messageStream) {
 			MemoryStream outputStream = new MemoryStream();
-			JsonRpcMessageSerializer messageSerializer = new JsonRpcMessageSerializer(Encoding.UTF8);
+			XmlRpcMessageSerializer messageSerializer = new XmlRpcMessageSerializer(Encoding.UTF8);
 			messageSerializer.Serialize(messageStream, outputStream);
 			outputStream.Position = 0;
 			StreamReader reader = new StreamReader(outputStream);
 			string line;
 			StringBuilder sb = new StringBuilder();
-			while ((line = reader.ReadLine()) != null) {
+			while((line = reader.ReadLine()) != null) {
 				sb.Append(line);
 			}
 			return sb.ToString();
 		}
-
+		
 		private Message Deserialize(string s, MessageType messageType) {
 			byte[] bytes = Encoding.UTF8.GetBytes(s);
 			MemoryStream inputStream = new MemoryStream(bytes);
 			XmlRpcMessageSerializer messageSerializer = new XmlRpcMessageSerializer(Encoding.UTF8);
 			return messageSerializer.Deserialize(inputStream, messageType);
 		}
-
+		
 		[Test]
 		public void SimpleMethodCallSerialize() {
 			RequestMessage request = new RequestMessage("testMethod");
@@ -38,9 +36,15 @@ namespace Deveel.Data.Net {
 			string s = Serialize(request);
 
 			StringBuilder expected = new StringBuilder();
-
-			//TODO:
-
+			expected.Append("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>");
+			expected.Append("<methodCall>");
+			expected.Append("<methodName>testMethod</methodName>");
+			expected.Append("<params>");
+			expected.Append("<param><value><i4>34</i4></value></param>");
+			expected.Append("<param><value><dateTime.iso8601>20090722T11:09:56</dateTime.iso8601></value></param>");
+			expected.Append("</params>");
+			expected.Append("</methodCall>");
+			
 			Console.Out.WriteLine("Generated:");
 			Console.Out.WriteLine(s);
 
@@ -50,8 +54,14 @@ namespace Deveel.Data.Net {
 		[Test]
 		public void SimpleMethodCallDeserialize() {
 			StringBuilder sb = new StringBuilder();
-
-			//TODO:
+			sb.Append("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>");
+			sb.Append("<methodCall>");
+			sb.Append("<methodName>testMethod</methodName>");
+			sb.Append("<params>");
+			sb.Append("<param><value><i4>34</i4></value></param>");
+			sb.Append("<param><value><dateTime.iso8601>20090722T11:09:56</dateTime.iso8601></value></param>");
+			sb.Append("</params>");
+			sb.Append("</methodCall>");
 
 			Message message = Deserialize(sb.ToString(), MessageType.Request);
 			Assert.AreEqual(MessageType.Request, message.MessageType);
@@ -143,7 +153,7 @@ namespace Deveel.Data.Net {
 		[Test]
 		public void ArraySerialize() {
 			RequestMessage request = new RequestMessage("testArray");
-			request.Arguments.Add(new int[] { 45, 87, 90, 112 });
+			request.Arguments.Add(new int[] {45, 87, 90, 112});
 
 			string s = Serialize(request);
 
@@ -171,7 +181,7 @@ namespace Deveel.Data.Net {
 			// mixed array
 
 			request = new RequestMessage("testMixedArray");
-			request.Arguments.Add(new object[] { 22, "test1", 56l, true });
+			request.Arguments.Add(new object[] {22, "test1", 56l, true});
 
 			s = Serialize(request);
 
@@ -223,7 +233,7 @@ namespace Deveel.Data.Net {
 
 			object value = request.Arguments[0].Value;
 			Assert.IsInstanceOf(typeof(int[]), value);
-			int[] array = (int[])value;
+			int [] array = (int[]) value;
 			Assert.AreEqual(4, array.Length);
 			Assert.AreEqual(45, array[0]);
 			Assert.AreEqual(87, array[1]);
@@ -254,7 +264,7 @@ namespace Deveel.Data.Net {
 
 			value = request.Arguments[0].Value;
 			Assert.IsInstanceOf(typeof(object[]), value);
-			object[] mixedArray = (object[])value;
+			object [] mixedArray = (object[])value;
 			Assert.AreEqual(4, mixedArray.Length);
 			Assert.AreEqual(22, mixedArray[0]);
 			Assert.AreEqual("test1", mixedArray[1]);
@@ -319,7 +329,7 @@ namespace Deveel.Data.Net {
 
 			Message message = Deserialize(sb.ToString(), MessageType.Request);
 			Assert.IsInstanceOf(typeof(RequestMessageStream), message);
-			RequestMessageStream requestStream = (RequestMessageStream)message;
+			RequestMessageStream requestStream = (RequestMessageStream) message;
 			Assert.AreEqual(2, requestStream.MessageCount);
 		}
 	}
---
+++ netsimul/Deveel.Data.Net/NetworkSimulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Deveel.Configuration;
+
+namespace Deveel.Data.Net {
+	public sealed class NetworkSimulator {
+		private static Options GetOptions() {
+			Options options = new Options();
+			options.AddOption("c", "conf", true, "A file containing the configurations to make the " + 
+			                  "instance of the network simulator.");
+			options.AddOption("");
+			return options;
+		}
+		
+		public static int Main(string[] args) {
+			
+			return 0;
+		}
+	}
+}
