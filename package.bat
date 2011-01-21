006b48085e33ad7c59e8c3b2f9b3aa6b40ebc4b6
Antonello Provenzano <antonello@deveel.com>
2011-01-21 16:26:26 +0000

    NAnt scripting and configuration.

--- package.bat
+++ package.bat
@@ -2,12 +2,12 @@
 IF "%~1" == "mono" GOTO MonoCompile ELSE GOTO DotNetCompile
 
 :DotNetCompile
-"%~dp0libs\nant\nant.exe" -buildfile:"%~dp0default.build" %*
+"%~dp0libs\nant\nant.exe" -buildfile:"%~dp0default.build" %* package
 GOTO End
 
 :MonoCompile
 SHIFT
-mono "%~dp0libs\nant\nant.exe" -buildfile:"%~dp0default.build" %*
+mono "%~dp0libs\nant\nant.exe" -buildfile:"%~dp0default.build" %* package
 echo %1
 GOTO End
 
