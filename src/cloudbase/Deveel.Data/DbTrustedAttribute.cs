dfbe8005cb901d0acbb9b6da5d25bf30004a2406
Antonello Provenzano <antonello@deveel.com>
2011-04-08 12:08:38 +0200

    Projects CloudB JSON and CloudBase dropped from the branch.

--- src/cloudbase/Deveel.Data/DbTrustedAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Deveel.Data {
-	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
-	public sealed class DbTrustedAttribute : Attribute {
-	}
-}
