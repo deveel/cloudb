dd39f9959fae790d25bbdaf82bd464f3af1c0259
Antonello Provenzano <antonello@deveel.com>
2011-07-24 00:44:13 +0200

    Fix of CloudB NUnit library

---
+++ src/cloudb-nunit/Deveel.Data.Diagnostics/LoggerTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using Deveel.Data.Configuration;
+
+using NUnit.Framework;
+
+namespace Deveel.Data.Diagnostics {
+	[TestFixture]
+	public class LoggerTest {
+		[TearDown]
+		public void TearDown() {
+			Logger.Clear();
+		}
+
+		[Test]
+		public void ConfigureDefaultDebugger() {
+			ConfigSource config = new ConfigSource();
+			config.SetValue("logger.foo.type", "default");
+
+			Logger.Init(config);
+			Logger logger = Logger.GetLogger("foo");
+			Assert.IsInstanceOf(typeof(DefaultLogger), logger.BaseLogger);
+		}
+		
+		[Test]
+		public void ConfigureSimpleConsoleLogger() {
+			ConfigSource config = new ConfigSource();
+			config.SetValue("logger.foo.type", "simple-console");
+			
+			Logger.Init(config);
+			Logger logger = Logger.GetLogger("foo");
+			Assert.IsInstanceOf(typeof(SimpleConsoleLogger), logger.BaseLogger);
+			
+			logger.Info("Printing a log message");
+		}
+		
+		[Test]
+		public void ConfigureNetworkLogger() {
+			ConfigSource config = new ConfigSource();
+			config.SetValue("logger.network.type", "simple-console");
+			
+			Logger.Init(config);
+			Logger logger = Logger.Network;
+			Assert.IsInstanceOf(typeof(SimpleConsoleLogger), logger.BaseLogger);
+		}
+		
+		[Test]
+		public void CompositeConsoleLogger() {
+			ConfigSource config = new ConfigSource();
+			config.SetValue("logger.composite.type", "composite");
+			config.SetValue("logger.composite.1.type", "simple-console");
+			config.SetValue("logger.composite.2.type", "simple-console");
+			
+			Logger.Init(config);
+			Logger logger = Logger.GetLogger("composite");
+			Assert.IsInstanceOf(typeof(CompositeLogger), logger.BaseLogger);
+			
+			logger.Info("Print a simple message that must be written in the console twice");
+		}
+
+		[Test]
+		public void DefaultWithoutExplicitSpec() {
+			ConfigSource config = new ConfigSource();
+			config.SetValue("logger.format", "[{Time}] - {Source} - {Message]");
+
+			Logger.Init(config);
+			Logger logger = Logger.GetLogger();
+			Assert.IsInstanceOf(typeof(DefaultLogger), logger.BaseLogger);
+			Assert.AreEqual("[{Time}] - {Source} - {Message]", logger.Config.GetString("format"));
+		}
+	}
+}
---
+++ src/cloudb-nunit/Deveel.Data.Net/TcpNetworkTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+using NUnit.Framework;
+
+namespace Deveel.Data.Net {
+	[TestFixture(NetworkStoreType.FileSystem)]
+	[TestFixture(NetworkStoreType.Memory)]
+	public sealed class TcpNetworkTest : NetworkTestBase {
+		private const string NetworkPassword = "123456";
+		
+		private static readonly TcpServiceAddress Local = new TcpServiceAddress(IPAddress.Loopback);
+
+		
+		public TcpNetworkTest(NetworkStoreType storeType)
+			: base(storeType) {
+		}
+
+		protected override IServiceAddress LocalAddress {
+			get { return Local; }
+		}
+
+		protected override AdminService CreateAdminService(NetworkStoreType storeType) {
+			IServiceFactory serviceFactory = null;
+			if (storeType == NetworkStoreType.Memory) {
+				serviceFactory = new MemoryServiceFactory();
+			} else if (storeType == NetworkStoreType.FileSystem) {
+				serviceFactory = new FileSystemServiceFactory(TestPath);
+			}
+
+			return new TcpAdminService(serviceFactory, Local, NetworkPassword);
+		}
+
+		protected override IServiceConnector CreateConnector() {
+			return new TcpServiceConnector(NetworkPassword);
+		}
+	}
+}
