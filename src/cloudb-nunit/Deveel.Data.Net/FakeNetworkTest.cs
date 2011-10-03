a8b818d5613643cdf400d67b66d5dbd77ec348ea
Antonello Provenzano <antonello@deveel.com>
2011-10-03 11:59:53 +0200

    Source control issues on NUnit project fixed

--- src/cloudb-nunit/Deveel.Data.Net/FakeNetworkTest.cs
+++ src/cloudb-nunit/Deveel.Data.Net/FakeNetworkTest.cs
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-
-using NUnit.Framework;
-
-namespace Deveel.Data.Net {
-	[TestFixture(NetworkStoreType.Memory)]
-	[TestFixture(NetworkStoreType.FileSystem)]
-	public class FakeNetworkTest : NetworkTestBase {		
-		public FakeNetworkTest(NetworkStoreType storeType)
-			: base(storeType) {
-		}
-
-		protected override IServiceAddress LocalAddress {
-			get { return FakeServiceAddress.Local; }
-		}
-
-		protected override AdminService CreateAdminService(NetworkStoreType storeType) {
-			return new FakeAdminService(storeType);
-		}
-
-		protected override IServiceConnector CreateConnector() {
-			return new FakeServiceConnector((FakeAdminService) AdminService);
-		}
-	}
-=======
-﻿using System;
+using System;
 
 using NUnit.Framework;
 
@@ -48,5 +22,4 @@ namespace Deveel.Data.Net {
 			return new FakeServiceConnector((FakeAdminService) AdminService);
 		}
 	}
->>>>>>> master
 }
--- src/cloudb-nunit/Deveel.Data.Net/HttpNetworkTest.cs
+++ src/cloudb-nunit/Deveel.Data.Net/HttpNetworkTest.cs
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-
-using Deveel.Data.Configuration;
-using Deveel.Data.Net.Serialization;
-
-using NUnit.Framework;
-
-namespace Deveel.Data.Net {
-	[TestFixture(HttpMessageFormat.Xml, NetworkStoreType.Memory)]
-	[TestFixture(HttpMessageFormat.Xml, NetworkStoreType.FileSystem)]
-	[TestFixture(HttpMessageFormat.Json, NetworkStoreType.Memory)]
-	[TestFixture(HttpMessageFormat.Json, NetworkStoreType.FileSystem)]
-    [Category("KnownUnstable")]
-	public sealed class HttpNetworkTest : NetworkTestBase {
-		private readonly HttpMessageFormat format;
-				
-		private static readonly HttpServiceAddress Local = new HttpServiceAddress("localhost", 1587);
-
-		
-		public HttpNetworkTest(HttpMessageFormat format, NetworkStoreType storeType)
-			: base(storeType) {
-			this.format = format;
-		}
-
-		protected override IServiceAddress LocalAddress {
-			get { return Local; }
-		}
-		
-		protected override AdminService CreateAdminService(NetworkStoreType storeType) {
-			IServiceFactory serviceFactory = null;
-			if (storeType == NetworkStoreType.Memory) {
-				serviceFactory = new MemoryServiceFactory();
-			} else if (storeType == NetworkStoreType.FileSystem) {
-				serviceFactory = new FileSystemServiceFactory(TestPath);
-			}
-
-			return new HttpAdminService(serviceFactory, Local);
-		}
-
-		protected override void Config(ConfigSource config) {
-			base.Config(config);
-
-			NetworkConfigSource networkConfig = (NetworkConfigSource) config;
-			networkConfig.AddAllowedIp("localhost");
-			networkConfig.AddAllowedIp("127.0.0.1");
-			networkConfig.AddAllowedIp("::1");
-		}
-
-		protected override IServiceConnector CreateConnector() {
-			HttpServiceConnector connector = new HttpServiceConnector();
-			if (format == HttpMessageFormat.Json)
-				connector.MessageSerializer = new JsonRpcMessageSerializer();
-			else if (format == HttpMessageFormat.Xml)
-				connector.MessageSerializer = new XmlRpcMessageSerializer();
-			return connector;
-		}
-	}
-=======
 ﻿using System;
 
 using Deveel.Data.Configuration;
@@ -114,5 +55,4 @@ namespace Deveel.Data.Net {
 			return connector;
 		}
 	}
->>>>>>> master
 }
